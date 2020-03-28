95e52818435aa5fde240e59dd35f1420fe361564
JuanCruz1997 <62308931+JuanCruz1997@users.noreply.github.com>
2020-03-28 19:07:56 -0300

    Objetos
    Realizado el objeto "Restaurante"

---
+++ Restaurante/ClassLibrary/Restaurant.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Restaurant
+    {
+        //Atributos
+        private string dueño;
+        private string direccion = "";
+        private bool ruidoso = false;
+        private int cantClientes = 0;
+
+        //Métodos
+        public string Dueño
+        {
+            set
+            {
+                this.dueño = value.ToUpper();
+            }
+            get
+            {
+                return this.dueño;
+            }
+        }
+        public string Direccion
+        {
+            set
+            {
+                this.direccion = value.ToUpper();
+            }
+            get
+            {
+                return this.direccion;
+            }
+        }
+        public bool Ruidoso
+        {
+            set
+            {
+                this.ruidoso = value;
+            }
+            get
+            {
+                return this.ruidoso;
+            }
+        }
+        public int CantClientes
+        {
+            set
+            {
+                this.cantClientes = value;
+            }
+            get
+            {
+                return this.cantClientes;
+            }
+        }
+        public string EstadoDeRuido()
+        {
+            if (Ruidoso)
+            {
+                return "Restaurante bullicioso";
+            }
+            else
+            {
+                return "Restaurante tranquilo";
+            }
+        }
+        public void CambiarEstadoDeRuido(int clientes)
+        {
+            if (clientes > 500)
+            {
+                Ruidoso = true;
+            }
+            else
+            {
+                Ruidoso = false;
+            }
+        }
+    }
+}
---
+++ Restaurante/Restaurante/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary;
+
+namespace Restaurante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido al restaurante");
+            Restaurant miRestaurante = new Restaurant();
+            miRestaurante.Dueño = "Juan Cruz Montes";
+            miRestaurante.Direccion = "Cdte. Franco 6100";
+            Console.WriteLine("Dueño: " + miRestaurante.Dueño + "\nDirección: " + miRestaurante.Direccion);
+            Console.WriteLine("Ingrese la cantidad de clientes del día:");
+            try
+            {
+                string comprobacion = Console.ReadLine();
+                int cantidad = int.Parse(comprobacion);
+                miRestaurante.CantClientes = cantidad;
+                miRestaurante.CambiarEstadoDeRuido(cantidad);
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine("La cantidad de clientes fue: " + miRestaurante.CantClientes);
+            Console.WriteLine(miRestaurante.EstadoDeRuido());
+            Console.ReadKey();
+        }
+    }
+}
